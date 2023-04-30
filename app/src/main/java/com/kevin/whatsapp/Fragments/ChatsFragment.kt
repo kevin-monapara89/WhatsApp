0f3c2e8fc34c15fdd5f094736fca4e9e398fbaa3
kevin-monapara89 <kevinmonapara89@gmail.com>
2023-04-30 13:18:20 +0530

    commit

--- app/src/main/java/com/kevin/whatsapp/Fragments/ChatsFragment.kt
+++ app/src/main/java/com/kevin/whatsapp/Fragments/ChatsFragment.kt
@@ -5,56 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kevin.whatsapp.Adapter.ChatAdapter
+import com.kevin.whatsapp.Model.ProfileModel
 import com.kevin.whatsapp.R
+import com.kevin.whatsapp.R.drawable.*
+import com.kevin.whatsapp.databinding.FragmentChatsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChatsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var image = arrayOf(dp1, dp2, dp3, dp4, dp5, dp6, dp7, dp8, dp9, dp10, dp11, dp12, dp13, dp14, dp15)
+    var name = arrayOf("Raman", "Crazy girl", "Shyam", "Rajesh", "Angel", "Sweeti", "Snaya", "Ajay", "Stan Boy", "Prete", "Semma", "Addie", "Srenu", "Rasmi", "Sam")
+
+    lateinit var binding : FragmentChatsBinding
+    var dataList = ArrayList<ProfileModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false)
-    }
+        binding = FragmentChatsBinding.inflate(layoutInflater)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+        for (x in 0..image.size-1) {
+            var data = ProfileModel(image[x], name[x])
+            dataList.add(data)
+        }
+
+        binding.rcvChatlist.layoutManager = LinearLayoutManager(context)
+        binding.rcvChatlist.adapter = ChatAdapter(dataList)
+
+        return binding.root
     }
 }
---
+++ app/src/main/java/com/kevin/whatsapp/Model/ProfileModel.kt
@@ -0,0 +1,12 @@
+package com.kevin.whatsapp.Model
+
+class ProfileModel {
+
+    var image : Int = 0
+    lateinit var name: String
+
+    constructor(image: Int, name: String) {
+        this.image = image
+        this.name = name
+    }
+}
